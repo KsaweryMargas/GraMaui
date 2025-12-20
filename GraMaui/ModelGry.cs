ac82a377b6e77b4171cfdff6a798d7dacc25c536
Ksawery Margas <ksawerymargas@gmail.com>
2025-12-20 22:48:27 +0100

    Dodano model gry

---
+++ GraMaui/ModelGry.cs
@@ -0,0 +1,8 @@
+﻿namespace GraMaui;
+
+public class ModelGry
+{
+    public int Id { get; set; }
+    public string Tytul { get; set; } = string.Empty;
+    public string Tag { get; set; } = string.Empty;
+}
