6a0a1201fc3e00daf031b54b2b373830cb8a7b9c
Ksawery Margas <ksawerymargas@gmail.com>
2025-12-21 17:53:36 +0100

    Dodano pobieranie danych gier i filtrowania po tagach

--- GraMaui/MainPage.xaml.cs
+++ GraMaui/MainPage.xaml.cs
@@ -12,6 +12,20 @@ namespace GraMaui
         public MainPage()
         {
             InitializeComponent();
+            DaneApi();
+        }
+
+        async void DaneApi()
+        {
+            PickerTagow.ItemsSource = await klient.GetFromJsonAsync<List<string>>("api/tags");
+            ListaGier.ItemsSource = await klient.GetFromJsonAsync<List<ModelGry>>("api/games");
+        }
+
+        async void ZmianaTag(object sender, EventArgs e)
+        {
+            var tag = PickerTagow.SelectedItem.ToString();
+            var url = $"api/games?tag={tag}";
+            ListaGier.ItemsSource = await klient.GetFromJsonAsync<List<ModelGry>>(url);
         }
     }
 }
