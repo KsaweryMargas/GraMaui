d9726c3ef208390e9197f615c56a12be8b6f65fa
Ksawery Margas <ksawerymargas@gmail.com>
2025-12-21 16:48:37 +0100

    Dodano połączenie z API

--- GraMaui/MainPage.xaml.cs
+++ GraMaui/MainPage.xaml.cs
@@ -1,24 +1,17 @@
-﻿namespace GraMaui
+﻿using System.Net.Http.Json;
+
+namespace GraMaui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        HttpClient klient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5092")
+        };
 
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
