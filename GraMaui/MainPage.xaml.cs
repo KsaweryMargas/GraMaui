55d64b9b0bc0db9e2f4c1a3ffef01b97e84b69b0
Ksawery Margas <ksawerymargas@gmail.com>
2026-01-12 00:06:31 +0100

    Dodano komentarze

--- GraMaui/MainPage.xaml.cs
+++ GraMaui/MainPage.xaml.cs
@@ -4,6 +4,7 @@ namespace GraMaui
 {
     public partial class MainPage : ContentPage
     {
+        // Konfiguracja połączenia z serwerem API
         HttpClient klient = new HttpClient
         {
             BaseAddress = new Uri("http://localhost:5092")
@@ -12,19 +13,24 @@ namespace GraMaui
         public MainPage()
         {
             InitializeComponent();
-            DaneApi();
+            DaneApi(); // Pobierz dane na starcie
         }
 
         async void DaneApi()
         {
+            // Pobierz listę tagów do Pickera
             PickerTagow.ItemsSource = await klient.GetFromJsonAsync<List<string>>("api/tags");
+            // Pobierz wszystkie gry do listy
             ListaGier.ItemsSource = await klient.GetFromJsonAsync<List<ModelGry>>("api/games");
         }
 
         async void ZmianaTag(object sender, EventArgs e)
         {
+            // Pobierz tekst wybranego tagu
             var tag = PickerTagow.SelectedItem.ToString();
+            // Stwórz adres z filtrowaniem
             var url = $"api/games?tag={tag}";
+            // Odśwież listę gier według tagu
             ListaGier.ItemsSource = await klient.GetFromJsonAsync<List<ModelGry>>(url);
         }
     }
